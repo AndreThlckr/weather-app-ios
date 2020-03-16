469fc4e7b7737771fa6e6196c012105bafb6515b
AndreThlckr <52230112+AndreThlckr@users.noreply.github.com>
2020-03-16 08:36:17 -0300

    Pod files and .gitignore

---
+++ Pods/Target Support Files/Pods-weather-app-ios-weather-app-iosUITests/Pods-weather-app-ios-weather-app-iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weather_app_ios_weather_app_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_weather_app_ios_weather_app_iosUITests
+@end
